f73d41a579af2f3b7dee481d81baf0b167bdfc0a
JNZader <javierzader@hotmail.com>
2024-06-19 20:30:06 -0300

    ej 4 y 5

--- TP5/ej5/main.cpp
+++ TP5/ej5/main.cpp
@@ -11,13 +11,88 @@ D. Se deben averiguar cuál es el último número agregado a cierta cola. Ej. Ul
  */
 
 #include <iostream>
+#include <queue>
+#include <string>
 
-using namespace std;
+struct Persona {
+    std::string DNI;
+    char operacion; // 'D' para depósitos, 'E' para extracciones, 'O' para varios
+};
 
-/*
- * 
- */
+std::queue<Persona> colaDepositos;
+std::queue<Persona> colaExtracciones;
+std::queue<Persona> colaVarios;
+
+std::queue<Persona> colaInicial;
+
+void simularTurno() {
+    while (!colaInicial.empty()) {
+        Persona p = colaInicial.front();
+        colaInicial.pop();
+        switch (p.operacion) {
+        case 'D':
+            colaDepositos.push(p);
+            break;
+        case 'E':
+            colaExtracciones.push(p);
+            break;
+        case 'O':
+            colaVarios.push(p);
+            break;
+        default:
+            std::cerr << "Operación desconocida para DNI: " << p.DNI << std::endl;
+        }
+    }
+}
+
+void agregarPersona(const std::string& DNI, char operacion) {
+    Persona p = { DNI, operacion };
+    colaInicial.push(p);
+    std::cout << "Persona con DNI " << DNI << " agregada a la cola inicial para la operación " << operacion << std::endl;
+}
+
+void atender(std::queue<Persona>& cola) {
+    if (!cola.empty()) {
+        Persona p = cola.front();
+        cola.pop();
+        std::cout << "Atendiendo a persona con DNI: " << p.DNI << std::endl;
+    }
+    else {
+        std::cout << "No hay personas en la cola para atender." << std::endl;
+    }
+}
+
+std::string ultimoDNIEnCola(const std::queue<Persona>& cola) {
+    if (cola.empty()) {
+        return "No hay personas en la cola.";
+    }
+    else {
+        std::queue<Persona> copiaCola = cola;
+        Persona ultimo;
+        while (!copiaCola.empty()) {
+            ultimo = copiaCola.front();
+            copiaCola.pop();
+        }
+        return ultimo.DNI;
+    }
+}
+
 int main() {
+    agregarPersona("12345678A", 'D');
+    agregarPersona("87654321B", 'E');
+    agregarPersona("11223344C", 'O');
+    agregarPersona("44332211D", 'D');
+
+    simularTurno();
+
+    std::cout << "Atendiendo cola de depósitos:" << std::endl;
+    atender(colaDepositos);
+    std::cout << "Atendiendo cola de extracciones:" << std::endl;
+    atender(colaExtracciones);
+    std::cout << "Atendiendo cola de varios:" << std::endl;
+    atender(colaVarios);
+
+    std::cout << "Último DNI en la cola de extracciones: " << ultimoDNIEnCola(colaExtracciones) << std::endl;
 
     return 0;
 }
